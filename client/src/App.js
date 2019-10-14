c9bff4c2bf46babc5b37546e6154740f67afa534
Jash <jash@Jashs-MBP.lan1>
2019-10-13 23:36:23 -0700

    Airline search on country frontend completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
-
+ 
+import {
+  Container,
+  Navbar,
+  NavbarBrand,
+  Row,
+  Jumbotron,
+  InputGroup,
+  InputGroupAddon,
+  Button,
+  Input,
+  Col
+} from 'reactstrap';
 
 function App() {
   return (
-    <div className="App">
- 
-    </div>
+    <Container fluid className="centered">
+    <Navbar dark color="dark">
+      <NavbarBrand href="/">Airline Search</NavbarBrand>
+    </Navbar>
+    <Row>
+      <Col>
+        <Jumbotron>
+          <h1 className="display-3">Airline Search</h1>
+          <p className="lead">Airline Search from Open flight Database!</p>
+          <InputGroup>
+            <Input placeholder="Enter Country Name"/>
+            <InputGroupAddon addonType="append">
+              <Button color="primary" >Get Airlines</Button>
+            </InputGroupAddon>
+            
+          </InputGroup>
+        </Jumbotron>
+      </Col>
+    </Row>
+  </Container>
   );
 }
 
